6b827266f13999fd585cffc7a6605f3b2223b11f
akshaybalirampimpale <akshaypimpale9@gmail.com>
2022-04-11 23:53:48 +0530

    Ready for deployment to heroku.

--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ def register(request):
         if form.is_valid():
             new_user = form.save()
             login(request, new_user)
-            return redirect('learning_logs:index')
+            return redirect('daily_logs:index')
 
     context = {'form': form}
     return render(request, 'registration/register.html', context)
